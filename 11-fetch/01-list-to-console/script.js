97e2942a4b4191222ed41d8c1dc3596a1eb7ac47
fourneauxthibaut <fourneaux.thibaut@gmail.com>
2021-09-24 17:58:27 +0200

    add

--- 11-fetch/01-list-to-console/script.js
+++ 11-fetch/01-list-to-console/script.js
@@ -2,5 +2,22 @@
 
 
 (() => {
-    // your code here
+    // your code here    
+    
+/*     async function fetchThis(url) {
+        const heroes = await fetch(url)
+            .then(response => {
+                const data = await heroes.json();
+                console.log(data);
+            });
+    }
+
+    document.getElementById('run').addEventListener('click', fetchThis( 'http://localhost:3000/heroes' )); */
+
+    document.getElementById("run").addEventListener("click", async () => {
+        const heroes = await fetch('http://localhost:3000/heroes');
+        const data = await heroes.json();
+        console.log(data);
+    })
+
 })();
--- 11-fetch/02-list-to-template/script.js
+++ 11-fetch/02-list-to-template/script.js
@@ -2,4 +2,32 @@
 
 (() => {
     // your code here
+
+    document.getElementById("run").addEventListener("click", async () => {
+        const heroes = await fetch('http://localhost:3000/heroes');
+        const data = await heroes.json();
+        const count = Object.keys(data).length;
+
+
+        if ("content" in document.createElement("template")) {
+            const template = document.getElementById('tpl-hero');
+            const target = document.getElementById("target");
+            const clone = [];
+        
+            for ( let i = 0; i < count; i++ ){
+                console.log(data[i]);
+                clone[i] = document.importNode(template.content, true);
+                let name = clone[i].querySelectorAll(".name");
+                let alterEgo = clone[i].querySelectorAll(".alter-ego");
+                let powers = clone[i].querySelectorAll(".powers");
+
+                name.textContent = data[i].name;
+                alterEgo.textContent = data[i].alterEgo;
+                powers.textContent = data[i].abilities;
+                
+                target.appendChild(clone[i]);
+            }     
+        }
+        console.log(data);
+    })
 })();
